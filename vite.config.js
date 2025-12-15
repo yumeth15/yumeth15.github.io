53eed4795686fadc2ee28cc8b4e37a9b849d5ad7
yumeth15 <67379412+yumeth15@users.noreply.github.com>
2025-12-15 11:28:11 +0530

    Update portfolio

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/yumeth15/yumeth15.github.io',
+
 })
